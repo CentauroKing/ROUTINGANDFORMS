b8e2a4cde6e22f73d89cefe13ee57bec9cee0e0c
CentauroKing <valencia.p.marco.v@gmail.com>
2025-07-04 18:45:01 -0500

    Primera parte hecha en clases

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { FormNormal} from './form-normal/form-normal';
+import { FormRectivo } from './form-rectivo/form-rectivo';
 
-export const routes: Routes = [];
+
+
+export const routes: Routes = [
+{path: '',
+    title: 'Fomulario Normal',
+    component: FormNormal,
+},
+{path: 'form-reactive',
+    title: 'Fomulario Reactivo',
+    component: FormRectivo,
+},
+
+];
